8736ed46241dd44df2bb0480663c80d099650ea4
CDA <CDA@STATION-Uo8g2QJ.exchange.ad.afpanet>
2025-05-22 16:18:05 +0200

    aside element

---
+++ src/data/fake_youtube_data.js
@@ -0,0 +1,12 @@
+import { TiThLarge } from "react-icons/ti";
+
+id, 
+img,
+duration,
+thumbnail,
+title,
+creator,
+certified,
+views,
+time,
+live,
---
+++ src/ui/AsideSummaryElement.js
@@ -0,0 +1,15 @@
+export default function AsideSummaryElement() {
+  return (
+    <div className="flex items-center gap-3 hover:bg-gray-100 hover:rounded-lg p-2 w-fit pr-16 hover:font-semibold">
+      <LuMessageSquareWarning className="w-7 h-7" />
+      <div>Envoyer des commentaires</div>
+    </div>
+  );
+}
+
+[
+id: ;
+img: ;
+img-color: ;
+title:;
+]
